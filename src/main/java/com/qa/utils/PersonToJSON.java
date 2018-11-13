f75425e0333a61f739278afe93e76ab256b21cc0
ross lawrence <ross.g.lawrence@gmail.com>
2018-11-13 15:15:41 +0000

    6th Commit Prettyfied JSON Out

--- src/main/java/com/qa/utils/PersonToJSON.java
+++ src/main/java/com/qa/utils/PersonToJSON.java
@@ -6,7 +6,7 @@ import com.qa.app.domain.Account;
 public class PersonToJSON {
 	public static String convertPerson(Account incomingAccount) throws JsonProcessingException {
 		ObjectMapper mapper = new ObjectMapper();
-		String JsonString = mapper.writeValueAsString(incomingAccount);
+		String JsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(incomingAccount);
 		return JsonString;
 	}
 }
