f5e25e211bbf2bfe05af03c37d96146aa3805ab2
ross lawrence <ross.g.lawrence@gmail.com>
2018-11-13 15:39:30 +0000

    Second Commit -- Tests Pass

--- src/main/java/com/qa/app/CountUser.java
+++ src/main/java/com/qa/app/CountUser.java
@@ -1,10 +1,29 @@
 package com.qa.app;
 
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import com.qa.app.domain.Account;
+
 public class CountUser {
 
-	public int countForFirstName(String string) {
-		// TODO Auto-generated method stub
-		return -1;
+	public int countForFirstName(String incomingName) {
+		Service s = new Service();
+		int temp = 0;
+		try {
+		Set<Entry<Integer, Account>> set = s.getHmap().entrySet();
+	      Iterator<Entry<Integer, Account>> iterator = set.iterator();
+	      while(iterator.hasNext()) {
+	         Entry<Integer, Account> mentry = iterator.next();
+	         if (mentry.getValue().getFirstName().equals(incomingName)) {
+	        	temp++;
+	         }
+	      }
+		} catch(NullPointerException npe) {
+			System.out.println(npe.toString());
+		}
+		return temp;
 	}
 
 }
